954c65229334b21ca82c49bd74943b7873133e49
Ivan winter <ivan.winter09@gmail.com>
2022-05-24 16:51:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
-let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+const randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-let randomDiceImage = "dice" + randomNumber1 + ".png";
+const randomDiceImage = "dice" + randomNumber1 + ".png";
 
-let randomImageSource = "images/" + randomDiceImage;
+const randomImageSource = "images/" + randomDiceImage;
 
-let image1 = document.querySelectorAll("img")[0];
+const image1 = document.querySelectorAll("img")[0];
 
 image1.setAttribute("src", randomImageSource);
 
 // --
 
-let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+const randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-let randomDiceImage2 = "dice" + randomNumber2 + ".png";
+const randomDiceImage2 = "dice" + randomNumber2 + ".png";
 
-let randomImageSource2 = "images/" + randomDiceImage2;
+const randomImageSource2 = "images/" + randomDiceImage2;
 
-let image2 = document.querySelectorAll("img")[1];
+const image2 = document.querySelectorAll("img")[1];
 
 image2.setAttribute("src", randomImageSource2);
-
-console.log(randomNumber1);
-console.log(randomNumber2);
-
 
 if (randomNumber1 === randomNumber2){
     document.querySelector("h1").innerHTML = "Draw!";
@@ -51,7 +47,6 @@ if (randomNumber1 === randomNumber2){
 // }
 
 // let randomNumber2 = Math.floor((Math.random()) * 6 + 1);
-// console.log(randomNumber2);
 
 // if(randomNumber2 === 1){
 //     document.querySelector(".img2").setAttribute("src", "/images/dice1.png");
